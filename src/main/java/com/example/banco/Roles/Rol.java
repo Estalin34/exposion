e2b3610ad291ac19ed310ebe5bf4d45f5f9ef0fd
Estalin34 <estalinleon06@hotmail.com>
2024-09-13 00:02:37 -0500

    expos

--- src/main/java/com/example/banco/Roles/Rol.java
+++ src/main/java/com/example/banco/Roles/Rol.java
@@ -1,8 +1,16 @@
 package com.example.banco.Roles;
 
-
-
 public enum Rol {
-    USER,
-    ADMIN;
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String nombreRol;
+
+    Rol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
 }
--- src/main/java/com/example/banco/Seguridad/SeguridadWeb.java
+++ src/main/java/com/example/banco/Seguridad/SeguridadWeb.java
@@ -1,15 +1,13 @@
 package com.example.banco.Seguridad;
 
-
 import com.example.banco.Servicios.UsuarioServicio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,40 +16,48 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(prePostEnabled = true)
 public class SeguridadWeb {
 
-    @Autowired
-    public UsuarioServicio usuarioServicio;
+    private final UsuarioServicio usuarioServicio;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(usuarioServicio)
-                .passwordEncoder(new BCryptPasswordEncoder());
+    public SeguridadWeb(UsuarioServicio usuarioServicio) {
+        this.usuarioServicio = usuarioServicio;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/admin/*").hasRole("ADMIN")
-                                .requestMatchers("/css/**", "/js/**", "/img/**",
-                                        "/registro", "/registrar").permitAll()  // Permite acceso a recursos estáticos
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/css/**", "/js/**", "/img/**", "/registro", "/registrar").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
                         formLogin
-                                .loginPage("/login")  // Página de inicio de sesión personalizada
-                                .loginProcessingUrl("/login")  // URL de procesamiento del inicio de sesión
-                                .usernameParameter("email")  // Parámetro para el nombre de usuario
-                                .passwordParameter("password")  // Parámetro para la contraseña
-                                .defaultSuccessUrl("/inicio")  // URL de éxito después del inicio de sesión
-                                .permitAll()  // Permite acceso a la página de inicio de sesión y otras URLs sin autenticación
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .usernameParameter("email")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/inicio", true)
+                                .permitAll()
                 )
                 .logout(logout ->
-                        logout.logoutUrl("/logout")
-                                .logoutSuccessUrl("/login")
-                                .permitAll() // Permite acceso a la funcionalidad de cierre de sesión sin autenticación
-                );
+                        logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                )
+                .csrf(csrf -> csrf.disable()); // Recomendación: Habilitar CSRF en producción si no hay razones específicas para desactivarlo
+
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
